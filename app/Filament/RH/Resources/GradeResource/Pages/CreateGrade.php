e1a6afe05921df92efec7d2e2d003c5367514543
sandrine <sandrine.zambelli@intradef.gouv.fr>
2025-07-16 11:04:11 +0000

    revision grades

--- app/Filament/RH/Resources/GradeResource/Pages/CreateGrade.php
+++ app/Filament/RH/Resources/GradeResource/Pages/CreateGrade.php
@@ -5,8 +5,23 @@ namespace Modules\RH\Filament\RH\Resources\GradeResource\Pages;
 use Modules\RH\Filament\RH\Resources\GradeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Modules\RH\Models\Grade;
 
 class CreateGrade extends CreateRecord
 {
     protected static string $resource = GradeResource::class;
+    protected static ?string $title = 'Créer grade';
+
+    // faire en sorte que l'ordre se décale si insertion au milieu.
+    protected function handleRecordCreation(array $data): Model
+    {
+        $ordre=$data['ordre'];
+        $list_grades=Grade::where('ordre', '>=' , $ordre)->get();
+        foreach($list_grades as $grade){
+            $grade->ordre ++;
+            $grade->save();
+        }
+        return static::getModel()::create($data);
+    }
 }
